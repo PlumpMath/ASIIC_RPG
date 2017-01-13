25f6162e725e8e3f3d53aa154c5ede38ba23ab38
mindoo <leon2@sautour.net>
2017-01-13 23:39:10 +0100

    Hello world

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <ncurses.h>
+#include <iostream>
 
 int main() {
+
+  initscr(); //Start curse mode
+  printw("Hello world"); //Print hello world
+  refresh(); //Print it on the real screen
+  getch(); //Wait for user input
+  endwin(); //End curse mode
 
   return 0;
 }
